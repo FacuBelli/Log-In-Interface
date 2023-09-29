110967f930e5b51a0f47f1902827c8db58f1bf0d
FacuBelli <facundo_carabelli@hotmail.com>
2023-09-29 17:51:18 -0300

    Comienzo validaciones

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('form').addEventListener('submit', function(event)) {
+    var username = document.getElementById('username').ariaValueMax;
+    var password = document.getElementById('password').ariaValueMax;
+}
