4c95524d9e273f0a24328913b87751b06ad138c4
FacuBelli <facundo_carabelli@hotmail.com>
2023-09-29 22:11:09 -0300

    Inicio registro

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-document.getElementById('form').addEventListener('submit', function(event)) {
-    var username = document.getElementById('username').ariaValueMax;
-    var password = document.getElementById('password').ariaValueMax;
-}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById('login-button').addEventListener('click', function () {
+
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+
+        // Aquí debes realizar tu lógica de validación
+        // Por ejemplo, podrías comprobar si ambos campos están llenos
+        if (username.trim() === '' || password.trim() === '') {
+            alert('Por favor, completa todos los campos.');
+        } else {
+
+            alert('Datos válidos. ¡Inicio de sesión exitoso!');
+        }
+    });
+
+
+    document.getElementById('cancel-button').addEventListener('click', function () {
+        alert('Inicio de sesión cancelado.');
+    });
+});
